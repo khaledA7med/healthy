5e7165548c4f4a71e666129ee02255d2dbb2e6c6
Mohamed Farghal <mohamedfarghal62@outlook.com>
2023-01-25 12:04:17 +0200

    [Update] Client Form

--- src/app/shared/app/routers/ApiRoutes.ts
+++ src/app/shared/app/routers/ApiRoutes.ts
@@ -1,16 +1,16 @@
 export const ApiRoutes = {
-	Users: {
-		login: "/Account/login",
-	},
-	Clients: {
-		search: "/ClientRegistry/Clients/Search",
-	},
-	ClientsGroups: {
-		list: "/v1/ClientRegistry/Groups/Search",
-		create: "/v1/ClientRegistry/Groups/Create",
-		delete: "/v1/ClientRegistry/Groups/Delete",
-		listGroupClients: "/v1/ClientRegistry/Groups/AllClients",
-		addGroupClient: "/v1/ClientRegistry/Groups/Join",
-		deleteGroupClient: "/v1/ClientRegistry/Groups/RemoveClient",
-	},
+  Users: {
+    login: "/Account/login",
+  },
+  Clients: {
+    search: "/ClientRegistry/Clients/Search",
+  },
+  ClientsGroups: {
+    list: "/ClientRegistry/Groups/Search",
+    create: "/v1/ClientRegistry/Groups/Create",
+    delete: "/v1/ClientRegistry/Groups/Delete",
+    listGroupClients: "/ClientRegistry/Groups/AllClients",
+    addGroupClient: "/v1/ClientRegistry/Groups/Join",
+    deleteGroupClient: "/v1/ClientRegistry/Groups/RemoveClient",
+  },
 };
