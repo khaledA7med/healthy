8c1a64b041d8174fcadd0b553b4fb94981829c42
khaledA7med <khaledcr22@gmail.com>
2023-01-29 20:03:40 +0200

    [Add] Change hijri date to gregorian date

--- src/app/shared/components/gregorian-picker/gregorian-picker.component.ts
+++ src/app/shared/components/gregorian-picker/gregorian-picker.component.ts
@@ -1,19 +1,18 @@
-import { Subscription } from 'rxjs';
-import { Component, OnInit, Injectable, Input,EventEmitter, Output } from '@angular/core';
-import { NgbDateStruct, NgbInputDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Injectable, Input,EventEmitter, Output, OnChanges } from '@angular/core';
+import { NgbCalendarIslamicUmalqura, NgbDateStruct, NgbInputDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gregorian-picker',
   templateUrl: './gregorian-picker.component.html',
   styleUrls: ['./gregorian-picker.component.scss'],
-  providers:[NgbInputDatepickerConfig]
+  providers:[NgbInputDatepickerConfig, NgbCalendarIslamicUmalqura]
 })
 
 @Injectable()
-export class GregorianPickerComponent implements OnInit {
+export class GregorianPickerComponent implements OnInit, OnChanges {
   model!: NgbDateStruct;
   date!: { year: number; month: number };
-  Subscription!:Subscription;
+  @Input() hijriDate:any;
 
   @Input("currentDate") currentDate!: [
     year: number,
@@ -23,10 +22,14 @@ export class GregorianPickerComponent implements OnInit {
 
   @Output() dateChange: EventEmitter<any> = new EventEmitter()
 
-  constructor(config: NgbInputDatepickerConfig) { 
+  constructor(config: NgbInputDatepickerConfig, public test: NgbCalendarIslamicUmalqura) { 
     config.minDate = { year: 1100, month: 1, day: 1 };
     config.maxDate = { year: 1500, month: 12, day: 31 };
     config.placement = ["top-end", "top-start", "bottom-end", "bottom-start"];
+  }
+
+  ngOnChanges(): void {
+    // console.log(this.gregorianDate)
   }
 
   ngOnInit(): void {
--- src/app/shared/components/hijri-picker/hijri-picker.component.ts
+++ src/app/shared/components/hijri-picker/hijri-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable, Input, OnInit, OnChanges } from "@angular/core";
+import { Component, Injectable, Input, OnInit, OnChanges, EventEmitter, Output } from "@angular/core";
 import {
   NgbCalendar,
   NgbCalendarIslamicUmalqura,
@@ -63,6 +63,9 @@ export class HijriPickerComponent implements OnInit, OnChanges {
     month: number,
     day: number
   ];
+
+  @Output() dateChange: EventEmitter<any> = new EventEmitter()
+
   constructor(config: NgbInputDatepickerConfig, public test: NgbCalendar, public test2: NgbCalendarIslamicUmalqura) {
     config.minDate = { year: 1900, month: 1, day: 1 };
     config.maxDate = { year: 2099, month: 12, day: 31 };
@@ -80,4 +83,9 @@ export class HijriPickerComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void { }
+
+  onDateSelect(e:any){
+    console.log(e);
+    this.dateChange.emit(e)
+  }
 }
