1d9c560dce869075900de1022d6f03511beefbda
amirmmonir@gmail.com <amirmmonir@gmail.com>
2023-02-08 10:53:54 +0200

    [Add] Policy Preview Module

---
+++ src/app/shared/components/poilcy-preview/poilcy-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poilcy-preview',
+  templateUrl: './poilcy-preview.component.html',
+  styleUrls: ['./poilcy-preview.component.scss']
+})
+export class PoilcyPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
