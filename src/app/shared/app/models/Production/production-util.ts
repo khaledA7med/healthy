e1a8c6ac52114222b2976343362cac9f90365091
Mohamed Farghal <mohamedfarghal62@outlook.com>
2023-02-08 10:42:03 +0200

    []

--- src/app/shared/app/models/Production/production-util.ts
+++ src/app/shared/app/models/Production/production-util.ts
@@ -9,4 +9,25 @@ export enum issueType {
   endorsement = "endorsement",
 }
 
-export interface IPolicyRequests {}
+export interface IFilterByRequest {
+  dateFrom?: string;
+  dateTo?: string;
+  clientName?: string;
+}
+
+export interface IPolicyRequests {
+  requestNo?: string;
+  endorsType?: string;
+  policyNo?: string;
+  clientID?: string;
+  classOfBusiness?: string;
+  lineOfBusiness?: string;
+  status?: string;
+  policySerial?: string;
+  clientPolicySNo?: string;
+  clieNetPremiumntPolicySNo?: string;
+  vatPerc?: string;
+  policyFees?: string;
+  clientName?: string;
+  producer?: string;
+}
