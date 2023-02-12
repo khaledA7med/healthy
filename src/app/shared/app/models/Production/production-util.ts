0076fa66e0ad76ca79b70e9192748327f3897107
amirmmonir@gmail.com <amirmmonir@gmail.com>
2023-02-12 09:47:44 +0200

    [Update] Working on Currency Formatter

--- src/app/shared/app/models/Production/production-util.ts
+++ src/app/shared/app/models/Production/production-util.ts
@@ -1,52 +1,61 @@
 export enum searchBy {
-  client = "Client",
-  request = "Request",
+	client = "Client",
+	request = "Request",
 }
 
 export enum issueType {
-  new = "new",
-  renewal = "renewal",
-  endorsement = "endorsement",
+	new = "new",
+	renewal = "renewal",
+	endorsement = "endorsement",
+}
+
+export enum IPolicyStatus {
+	active = "Active",
+	pending = "Pending",
+	expired = "Expired",
+	approvedByProduction = "Approved by Production",
+	rejectedByProduction = "Rejected by Production",
+	rejectedByFinance = "Rejected by Finance",
 }
 
 export interface IFilterByRequest {
-  dateFrom?: string;
-  dateTo?: string;
-  clientName?: string;
+	dateFrom?: string;
+	dateTo?: string;
+	clientName?: string;
 }
 
 export interface IPolicyRequests {
-  requestNo?: string;
-  endorsType?: string;
-  policyNo?: string;
-  clientID?: string;
-  classOfBusiness?: string;
-  lineOfBusiness?: string;
-  status?: string;
-  policySerial?: string;
-  clientPolicySNo?: string;
-  clieNetPremiumntPolicySNo?: string;
-  vatPerc?: string;
-  policyFees?: string;
-  clientName?: string;
-  producer?: string;
+	requestNo?: string;
+	endorsType?: string;
+	policyNo?: string;
+	clientID?: string;
+	classOfBusiness?: string;
+	lineOfBusiness?: string;
+	status?: string;
+	policySerial?: string;
+	clientPolicySNo?: string;
+	clieNetPremiumntPolicySNo?: string;
+	vatPerc?: string;
+	policyFees?: string;
+	clientName?: string;
+	producer?: string;
 }
 
 export interface IPolicyClient {
-  sNo?: string;
-  status?: string;
-  fullName?: string;
-  producer?: string;
+	sNo?: string;
+	status?: string;
+	fullName?: string;
+	producer?: string;
 }
 
 export interface IPoliciesRef {
-  sNo: string;
-  insurComp: string;
-  className: string;
-  lineOfBusiness: string;
-  accNo: string;
-  policyNo: string;
-  periodFrom: string;
-  periodTo: string;
-  policiesSNo: string;
+	sNo: string;
+	insurComp: string;
+	className: string;
+	lineOfBusiness: string;
+	accNo: string;
+	policyNo: string;
+	periodFrom: string;
+	periodTo: string;
+	policiesSNo: string;
 }
