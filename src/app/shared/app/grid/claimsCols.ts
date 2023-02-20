e7f64eecbc832ad5d5cc2b852e307b7b2803eea7
Hassan Sabry Hassan <h.sabry@oasisoft.net>
2023-02-20 16:02:48 +0200

    [Create] Claims Filter

--- src/app/shared/app/grid/claimsCols.ts
+++ src/app/shared/app/grid/claimsCols.ts
@@ -1,5 +1,7 @@
 import { ClaimsControlsComponent } from "./../../../pages/claims/claims-list/claims-controls.component";
 import { ColDef } from "ag-grid-community";
+import StatusCellRender from "./statusCellRender";
+import GlobalCellRender from "./globalCellRender";
 export const claimsManageCols: ColDef[] = [
   {
     colId: "action",
@@ -11,5 +13,194 @@ export const claimsManageCols: ColDef[] = [
   {
     headerName: "Branch",
     field: "branch",
+  },
+  {
+    headerName: "Claim No",
+    field: "claimNo",
+    minWidth: 200,
+  },
+  {
+    headerName: "Status",
+    cellRenderer: StatusCellRender.claimStatus,
+    field: "status",
+  },
+  {
+    headerName: "Sub Status",
+    field: "claimStatusNotes",
+    minWidth: 200,
+  },
+  {
+    headerName: "Notify Client ",
+    field: "notifyClient",
+    cellRenderer: GlobalCellRender.NotifyChecker,
+  },
+  {
+    headerName: "Notify Insurer",
+    field: "notifyInsurer",
+    cellRenderer: GlobalCellRender.NotifyChecker,
+  },
+  {
+    headerName: "Client ID",
+    field: "clientID",
+  },
+  {
+    headerName: "Client Name",
+    field: "clientName",
+    minWidth: 200,
+  },
+  {
+    headerName: "Plat No",
+    field: "carNo",
+    minWidth: 120,
+  },
+  {
+    headerName: "Chassis No",
+    field: "chassisNumber",
+    minWidth: 200,
+  },
+  {
+    headerName: "Shipment Name",
+    field: "shipmentName",
+    minWidth: 200,
+  },
+  {
+    headerName: "model",
+    field: "model",
+    minWidth: 120,
+  },
+  {
+    headerName: "insurance Company",
+    field: "insuranceCompany",
+    minWidth: 230,
+  },
+  {
+    headerName: "Class Name",
+    field: "className",
+    minWidth: 120,
+  },
+  {
+    headerName: "line Of Business",
+    field: "lineofBusiness",
+    minWidth: 230,
+  },
+  {
+    headerName: "Policy No",
+    field: "policyNo",
+    minWidth: 280,
+  },
+  {
+    headerName: "Member/Drive Name",
+    field: "membName",
+    minWidth: 230,
+  },
+  {
+    headerName: "Claim Amount",
+    field: "claimAmount",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Paid Amount ",
+    field: "amountPaid",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Rejected",
+    field: "amountRejected",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Under Processing",
+    field: "underProcessingAmount",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Estimated Value",
+    field: "estimatedValue",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Total Approvals",
+    field: "totalApprovals",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Towing Changes (invoice)",
+    field: "towingCharges",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Amount Paid To Workshop",
+    field: "totalPaidToWorkshop",
+    valueFormatter: GlobalCellRender.currencyFormater,
+    minWidth: 200,
+  },
+  {
+    headerName: "Accident/Bill Date",
+    field: "dateOfLoss",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Notification Date",
+    field: "dateOfReceive",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Date Of Submission",
+    field: "dateOfSubmission",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Date of Deadline",
+    field: "dateOfDeadline",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Approval Data",
+    field: "approvalDate",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Invoice Date",
+    field: "invoiceDate",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Payment Date",
+    field: "dateofPayment",
+    valueFormatter: GlobalCellRender.dateFormater,
+    minWidth: 150,
+  },
+  {
+    headerName: "Ref#",
+    field: "insurCompClaimNo",
+    minWidth: 150,
+  },
+  {
+    headerName: "Notes",
+    field: "notes",
+    minWidth: 150,
+  },
+  {
+    headerName: "Created By ",
+    field: "savedUser",
+    minWidth: 150,
+  },
+  {
+    headerName: "Created On",
+    field: "savedDate",
+    minWidth: 150,
   },
 ];
--- src/app/shared/app/grid/statusCellRender.ts
+++ src/app/shared/app/grid/statusCellRender.ts
@@ -2,82 +2,90 @@ import { ClientStatus } from "./../models/Clients/clientUtil";
 import { CellEvent } from "ag-grid-community";
 import { SalesLeadStatus } from "../models/BusinessDevelopment/business-development-util";
 import { IPolicyStatus } from "../models/Production/production-util";
+import { claimsStatus } from "../models/Claims/claims-util";
 
-export default class StatusCellRender
-{
-	public static clientStatus (e: CellEvent): string
-	{
-		switch (e.value)
-		{
-			case ClientStatus.Prospect:
-				return `<span class='badge bg-warning'>${ e.value }</span>`;
-			case ClientStatus.Active:
-				return `<span class='badge bg-success'>${ e.value }</span>`;
-			case ClientStatus.PendingActivation:
-				return `<span class='badge bg-info'>${ e.value }</span>`;
-			case ClientStatus.Blocked:
-				return `<span class='badge bg-dark'>${ e.value }</span>`;
-			case ClientStatus.Rejected:
-				return `<span class='badge bg-danger'>${ e.value }</span>`;
-			default:
-				return `<span class='badge bg-success'>${ e.value }</span>`;
-		}
-	}
+export default class StatusCellRender {
+  public static clientStatus(e: CellEvent): string {
+    switch (e.value) {
+      case ClientStatus.Prospect:
+        return `<span class='badge bg-warning'>${e.value}</span>`;
+      case ClientStatus.Active:
+        return `<span class='badge bg-success'>${e.value}</span>`;
+      case ClientStatus.PendingActivation:
+        return `<span class='badge bg-info'>${e.value}</span>`;
+      case ClientStatus.Blocked:
+        return `<span class='badge bg-dark'>${e.value}</span>`;
+      case ClientStatus.Rejected:
+        return `<span class='badge bg-danger'>${e.value}</span>`;
+      default:
+        return `<span class='badge bg-success'>${e.value}</span>`;
+    }
+  }
 
-	public static salesLeadStatus (e: CellEvent): string
-	{
-		switch (e.value)
-		{
-			case SalesLeadStatus.Prospect:
-				return `<span class='badge bg-info'>${ e.value }</span>`;
-			case SalesLeadStatus.Confirmed:
-				return `<span class='badge bg-success'>${ e.value }</span>`;
-			case SalesLeadStatus.Quoting:
-				return `<span class='badge bg-warning'>${ e.value }</span>`;
-			case SalesLeadStatus.PendingwithUnderwriting:
-				return `<span class='badge bg-dark'>${ e.value }</span>`;
-			case SalesLeadStatus.Lost:
-				return `<span class='badge bg-danger'>${ e.value }</span>`;
-			case SalesLeadStatus.WaitingForClientFeedback:
-				return `<span class='badge bg-secondary'>${ e.value }</span>`;
-			default:
-				return `<span class='badge bg-success'>${ e.value }</span>`;
-		}
-	}
+  public static salesLeadStatus(e: CellEvent): string {
+    switch (e.value) {
+      case SalesLeadStatus.Prospect:
+        return `<span class='badge bg-info'>${e.value}</span>`;
+      case SalesLeadStatus.Confirmed:
+        return `<span class='badge bg-success'>${e.value}</span>`;
+      case SalesLeadStatus.Quoting:
+        return `<span class='badge bg-warning'>${e.value}</span>`;
+      case SalesLeadStatus.PendingwithUnderwriting:
+        return `<span class='badge bg-dark'>${e.value}</span>`;
+      case SalesLeadStatus.Lost:
+        return `<span class='badge bg-danger'>${e.value}</span>`;
+      case SalesLeadStatus.WaitingForClientFeedback:
+        return `<span class='badge bg-secondary'>${e.value}</span>`;
+      default:
+        return `<span class='badge bg-success'>${e.value}</span>`;
+    }
+  }
 
-	public static customerServiceStatus (e: CellEvent): string
-	{
-		switch (e.value)
-		{
-			case "Pending":
-				return `<span class='badge bg-info'>${ e.value }</span>`;
-			case "New Request":
-				return `<span class='badge bg-secondary'>${ e.value }</span>`;
-			case "Closed":
-				return `<span class='badge bg-success'>${ e.value }</span>`;
-			case "Canceled":
-				return `<span class='badge bg-danger'>${ e.value }</span>`;
-			default:
-				return `<span class='badge bg-dark'>${ e.value }</span>`;
-		}
-	}
+  public static customerServiceStatus(e: CellEvent): string {
+    switch (e.value) {
+      case "Pending":
+        return `<span class='badge bg-info'>${e.value}</span>`;
+      case "New Request":
+        return `<span class='badge bg-secondary'>${e.value}</span>`;
+      case "Closed":
+        return `<span class='badge bg-success'>${e.value}</span>`;
+      case "Canceled":
+        return `<span class='badge bg-danger'>${e.value}</span>`;
+      default:
+        return `<span class='badge bg-dark'>${e.value}</span>`;
+    }
+  }
 
-	public static policyStatus(e: CellEvent): string {
-		switch (e.value) {
-			case IPolicyStatus.active:
-				return `<span class='badge badge-soft-success'>${e.value}</span>`;
-			case IPolicyStatus.pending:
-				return `<span class='badge badge-soft-warning'>${e.value}</span>`;
-			case IPolicyStatus.expired:
-				return `<span class='badge badge-soft-danger'>${e.value}</span>`;
-			case IPolicyStatus.approvedByProduction:
-				return `<span class='badge badge-soft-info'>${e.value}</span>`;
-			case IPolicyStatus.rejectedByProduction:
-				return `<span class='badge badge-soft-danger'>${e.value}</span>`;
-			case IPolicyStatus.rejectedByFinance:
-				return `<span class='badge badge-soft-primary'>${e.value}</span>`;
-			default:
-				return `<span class='badge badge-soft-info'>${e.value}</span>`;
-		}
-	}
+  public static policyStatus(e: CellEvent): string {
+    switch (e.value) {
+      case IPolicyStatus.active:
+        return `<span class='badge badge-soft-success'>${e.value}</span>`;
+      case IPolicyStatus.pending:
+        return `<span class='badge badge-soft-warning'>${e.value}</span>`;
+      case IPolicyStatus.expired:
+        return `<span class='badge badge-soft-danger'>${e.value}</span>`;
+      case IPolicyStatus.approvedByProduction:
+        return `<span class='badge badge-soft-info'>${e.value}</span>`;
+      case IPolicyStatus.rejectedByProduction:
+        return `<span class='badge badge-soft-danger'>${e.value}</span>`;
+      case IPolicyStatus.rejectedByFinance:
+        return `<span class='badge badge-soft-primary'>${e.value}</span>`;
+      default:
+        return `<span class='badge badge-soft-info'>${e.value}</span>`;
+    }
+  }
+  public static claimStatus(e: CellEvent): string {
+    switch (e.value) {
+      case claimsStatus.active:
+        return `<span class='badge bg-success'>${e.value}</span>`;
+      case claimsStatus.pending:
+        return `<span class='badge bg-warning'>${e.value}</span>`;
+      case claimsStatus.closed:
+        return `<span class='badge bg-info'>${e.value}</span>`;
+      case claimsStatus.rejected:
+        return `<span class='badge bg-danger'>${e.value}</span>`;
+      default:
+        return `<span class='badge bg-black'>${e.value}</span>`;
+    }
+  }
 }
