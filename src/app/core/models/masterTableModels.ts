aae3489d0cf073b65452041dd02e4c6a9146432b
amirmmonir@gmail.com <amirmmonir@gmail.com>
2023-02-21 17:19:02 +0200

    [Update] Working On Add & Edit Users in Sys Admin

--- src/app/core/models/masterTableModels.ts
+++ src/app/core/models/masterTableModels.ts
@@ -1,127 +1,131 @@
 export enum BaseData {
-  ClientType = "ClientTypes",
-  Producers = "Producers",
-  RelationshipStatus = "RelationshipStatus",
-  BusinessType = "BusinessType",
-  Channels = "Channels",
-  Interface = "Interface",
-  ScreeningResult = "ScreeningResult",
-  Nationalities = "Nationalities",
-  SourceofIncome = "SourceofIncome",
-  RegistrationStatus = "RegistrationStatus",
-  BusinessActivities = "BusinessActivities",
-  MarketSegment = "MarketSegment",
-  Premium = "Premium",
-  Positions = "Positions",
-  Branch = "Branch",
-  LineOfBusiness = "LineOfBusiness",
-  ContactLineOfBusiness = "ContactLineOfBusiness",
-  ContactDepartment = "ContactDepartment",
-  Banks = "Banks",
-  CommericalNo = "CommericalNo",
-  GroupsList = "GroupsList",
-  ClientStatus = "ClientStatus",
-  InsurClasses = "InsurClasses",
-  AllUsers = "AllUsers",
-  SalesleadStatus = "SalesleadStatus",
-  PolicyStatus = "PolicyStatus",
-  PolicyEndorsTypes = "PolicyEndorsTypes",
-  ClientsList = "ClientsList",
-  InsuranceCompanies = "InsuranceCompanies",
-  CServiceStatus = "CServiceStatus",
-  PendingReason = "PendingReason",
-  TypeOfCustomerServices = "TypeOfCustomerServices",
-  AllClients = "AllClients",
-  InsuranceCompanyName = "InsuranceCompanyName",
-  InsuranceBrokersList = "InsuranceBrokersList",
-  LogType = "LogType",
-  ProductionFieldList = "ProductionFieldList",
-  ProductionOperatordList = "ProductionOperatordList",
-  VATPercentage = "VATPercentage",
-  ClaimStatus = "ClaimStatus",
+	ClientType = "ClientTypes",
+	Producers = "Producers",
+	RelationshipStatus = "RelationshipStatus",
+	BusinessType = "BusinessType",
+	Channels = "Channels",
+	Interface = "Interface",
+	ScreeningResult = "ScreeningResult",
+	Nationalities = "Nationalities",
+	SourceofIncome = "SourceofIncome",
+	RegistrationStatus = "RegistrationStatus",
+	BusinessActivities = "BusinessActivities",
+	MarketSegment = "MarketSegment",
+	Premium = "Premium",
+	Positions = "Positions",
+	Branch = "Branch",
+	LineOfBusiness = "LineOfBusiness",
+	ContactLineOfBusiness = "ContactLineOfBusiness",
+	ContactDepartment = "ContactDepartment",
+	Banks = "Banks",
+	CommericalNo = "CommericalNo",
+	GroupsList = "GroupsList",
+	ClientStatus = "ClientStatus",
+	InsurClasses = "InsurClasses",
+	AllUsers = "AllUsers",
+	SalesleadStatus = "SalesleadStatus",
+	PolicyStatus = "PolicyStatus",
+	PolicyEndorsTypes = "PolicyEndorsTypes",
+	ClientsList = "ClientsList",
+	InsuranceCompanies = "InsuranceCompanies",
+	CServiceStatus = "CServiceStatus",
+	PendingReason = "PendingReason",
+	TypeOfCustomerServices = "TypeOfCustomerServices",
+	AllClients = "AllClients",
+	InsuranceCompanyName = "InsuranceCompanyName",
+	InsuranceBrokersList = "InsuranceBrokersList",
+	LogType = "LogType",
+	ProductionFieldList = "ProductionFieldList",
+	ProductionOperatordList = "ProductionOperatordList",
+	VATPercentage = "VATPercentage",
+	ClaimStatus = "ClaimStatus",
+	JobTitleOfUsers = "JobTitleOfUsers",
+	StatusOfUsers = "StatusOfUsers",
 }
 
 export interface IBaseMasterTable {
-  ClientTypes?: Caching<IGenericResponseType[]>;
-  Producers?: Caching<IGenericResponseType[]>;
-  RelationshipStatus?: Caching<IGenericResponseType[]>;
-  BusinessType?: Caching<IGenericResponseType[]>;
-  Channels?: Caching<IGenericResponseType[]>;
-  Interface?: Caching<IGenericResponseType[]>;
-  ScreeningResult?: Caching<IGenericResponseType[]>;
-  Nationalities?: Caching<INationality[]>;
-  SourceofIncome?: Caching<IGenericResponseType[]>;
-  RegistrationStatus?: Caching<IGenericResponseType[]>;
-  BusinessActivities?: Caching<IBusinessActivity[]>;
-  MarketSegment?: Caching<IGenericResponseType[]>;
-  Premium?: Caching<IGenericResponseType[]>;
-  Positions?: Caching<IPositions[]>;
-  Branch?: Caching<IGenericResponseType[]>;
-  ContactLineOfBusiness?: Caching<IGenericResponseType[]>;
-  ContactDepartment?: Caching<IGenericResponseType[]>;
-  Banks?: Caching<IBanks[]>;
-  CommericalNo?: Caching<IGenericResponseType[]>;
-  GroupsList?: Caching<IGenericResponseType[]>;
-  ClientStatus?: Caching<IGenericResponseType[]>;
-  CSRegistryClients?: Caching<IGenericResponseType[]>;
-  CServiceStatus?: Caching<IGenericResponseType[]>;
-  PendingReason?: Caching<IGenericResponseType[]>;
-  AllUsers?: Caching<IGenericResponseType[]>;
-  TypeOfCustomerServices?: Caching<IGenericResponseType[]>;
-  SalesleadStatus?: Caching<IGenericResponseType[]>;
-  PolicyEndorsTypes?: Caching<IGenericResponseType[]>;
-  ClientsList?: Caching<IGenericResponseType[]>;
-  InsuranceCompanies?: Caching<IGenericResponseType[]>;
-  PolicyStatus?: Caching<IGenericResponseType[]>;
-  AllClients?: Caching<IGenericResponseType[]>;
-  InsurClasses?: Caching<IGenericResponseType[]>;
-  InsuranceCompanyName?: Caching<IGenericResponseType[]>;
-  InsuranceBrokersList?: Caching<IGenericResponseType[]>;
-  LogType?: Caching<IGenericResponseType[]>;
-  ProductionFieldList?: Caching<IGenericResponseType[]>;
-  ProductionOperatordList?: Caching<IGenericResponseType[]>;
-  VATPercentage?: Caching<IGenericResponseType[]>;
-  ClaimStatus?: Caching<IGenericResponseType[]>;
+	ClientTypes?: Caching<IGenericResponseType[]>;
+	Producers?: Caching<IGenericResponseType[]>;
+	RelationshipStatus?: Caching<IGenericResponseType[]>;
+	BusinessType?: Caching<IGenericResponseType[]>;
+	Channels?: Caching<IGenericResponseType[]>;
+	Interface?: Caching<IGenericResponseType[]>;
+	ScreeningResult?: Caching<IGenericResponseType[]>;
+	Nationalities?: Caching<INationality[]>;
+	SourceofIncome?: Caching<IGenericResponseType[]>;
+	RegistrationStatus?: Caching<IGenericResponseType[]>;
+	BusinessActivities?: Caching<IBusinessActivity[]>;
+	MarketSegment?: Caching<IGenericResponseType[]>;
+	Premium?: Caching<IGenericResponseType[]>;
+	Positions?: Caching<IPositions[]>;
+	Branch?: Caching<IGenericResponseType[]>;
+	ContactLineOfBusiness?: Caching<IGenericResponseType[]>;
+	ContactDepartment?: Caching<IGenericResponseType[]>;
+	Banks?: Caching<IBanks[]>;
+	CommericalNo?: Caching<IGenericResponseType[]>;
+	GroupsList?: Caching<IGenericResponseType[]>;
+	ClientStatus?: Caching<IGenericResponseType[]>;
+	CSRegistryClients?: Caching<IGenericResponseType[]>;
+	CServiceStatus?: Caching<IGenericResponseType[]>;
+	PendingReason?: Caching<IGenericResponseType[]>;
+	AllUsers?: Caching<IGenericResponseType[]>;
+	TypeOfCustomerServices?: Caching<IGenericResponseType[]>;
+	SalesleadStatus?: Caching<IGenericResponseType[]>;
+	PolicyEndorsTypes?: Caching<IGenericResponseType[]>;
+	ClientsList?: Caching<IGenericResponseType[]>;
+	InsuranceCompanies?: Caching<IGenericResponseType[]>;
+	PolicyStatus?: Caching<IGenericResponseType[]>;
+	AllClients?: Caching<IGenericResponseType[]>;
+	InsurClasses?: Caching<IGenericResponseType[]>;
+	InsuranceCompanyName?: Caching<IGenericResponseType[]>;
+	InsuranceBrokersList?: Caching<IGenericResponseType[]>;
+	LogType?: Caching<IGenericResponseType[]>;
+	ProductionFieldList?: Caching<IGenericResponseType[]>;
+	ProductionOperatordList?: Caching<IGenericResponseType[]>;
+	VATPercentage?: Caching<IGenericResponseType[]>;
+	ClaimStatus?: Caching<IGenericResponseType[]>;
+	JobTitleOfUsers?: Caching<IGenericResponseType[]>;
+	StatusOfUsers?: Caching<IGenericResponseType[]>;
 }
 
 export interface Caching<T> {
-  cacheable: boolean;
-  content: T;
+	cacheable: boolean;
+	content: T;
 }
 
 export interface IAllClients {
-  sNo: number;
-  fullName: string;
-  producer: string;
-  status: string;
+	sNo: number;
+	fullName: string;
+	producer: string;
+	status: string;
 }
 
 export interface IGenericResponseType {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 export interface INationality {
-  nationalityEn?: string;
-  nationalityAr?: string;
+	nationalityEn?: string;
+	nationalityAr?: string;
 }
 
 export interface IBusinessActivity {
-  sNo: number;
-  businessActivity: string;
+	sNo: number;
+	businessActivity: string;
 }
 
 export interface IPositions {
-  sNo: number;
-  position: string;
+	sNo: number;
+	position: string;
 }
 export interface IBanks {
-  sNo?: number;
-  bankName?: string;
-  swift?: string;
+	sNo?: number;
+	bankName?: string;
+	swift?: string;
 }
 
 export interface IClientTypes {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
--- src/app/pages/system-admin/user-accounts-management/user-accounts-management.component.ts
+++ src/app/pages/system-admin/user-accounts-management/user-accounts-management.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
+import { Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from "@angular/core";
 import { Router } from "@angular/router";
 import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 import { CellEvent, GridApi, GridOptions, GridReadyEvent, IDatasource, IGetRowsParams } from "ag-grid-community";
 import { Observable, Subscription } from "rxjs";
-import { NgbOffcanvas } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal, NgbModalRef, NgbOffcanvas } from "@ng-bootstrap/ng-bootstrap";
 
 import PerfectScrollbar from "perfect-scrollbar";
 import { AppRoutes } from "src/app/shared/app/routers/appRouters";
@@ -19,215 +19,241 @@ import { ISystemAdmin } from "src/app/shared/app/models/SystemAdmin/isystem-admi
 import { systemAdminCols } from "src/app/shared/app/grid/systemAdminCols";
 import { SystemAdminService } from "src/app/shared/services/system-admin/system-admin.service";
 import { MasterMethodsService } from "src/app/shared/services/master-methods.service";
-
+import { EventService } from "src/app/core/services/event.service";
+import { UserModel } from "src/app/shared/app/models/SystemAdmin/isystem-admin-user";
 
 @Component({
-  selector: 'app-user-accounts-management',
-  templateUrl: './user-accounts-management.component.html',
-  styleUrls: [ './user-accounts-management.component.scss' ],
-  providers: [ AppUtils ],
-  encapsulation: ViewEncapsulation.None,
+	selector: "app-user-accounts-management",
+	templateUrl: "./user-accounts-management.component.html",
+	styleUrls: ["./user-accounts-management.component.scss"],
+	providers: [AppUtils],
+	encapsulation: ViewEncapsulation.None,
 })
-export class UserAccountsManagementComponent implements OnInit, OnDestroy
-{
+export class UserAccountsManagementComponent implements OnInit, OnDestroy {
+	uiState = {
+		routerLink: {
+			forms: AppRoutes.SystemAdmin.create,
+		},
+		filters: {
+			pageNumber: 1,
+			pageSize: 50,
+			orderBy: "sno",
+			orderDir: "asc",
+		} as ISystemAdminFilters,
+		gridReady: false,
+		submitted: false,
+		admins: {
+			list: [] as ISystemAdmin[],
+			totalPages: 0,
+		},
+		filterByAmount: false,
+	};
 
-  uiState = {
-    routerLink: {
-      forms: AppRoutes.SystemAdmin.create,
-    },
-    filters: {
-      pageNumber: 1,
-      pageSize: 50,
-      orderBy: "sno",
-      orderDir: "asc"
-    } as ISystemAdminFilters,
-    gridReady: false,
-    submitted: false,
-    admins: {
-      list: [] as ISystemAdmin[],
-      totalPages: 0,
-    },
-    filterByAmount: false,
-  };
+	filterForm!: FormGroup;
+	lookupData!: Observable<IBaseMasterTable>;
+	@ViewChild("filter") policiesFilter!: ElementRef;
 
-  filterForm!: FormGroup;
-  lookupData!: Observable<IBaseMasterTable>;
-  @ViewChild("filter") policiesFilter!: ElementRef;
+	subscribes: Subscription[] = [];
+	gridApi: GridApi = <GridApi>{};
+	gridOpts: GridOptions = {
+		pagination: true,
+		rowModelType: "infinite",
+		editType: "fullRow",
+		animateRows: true,
+		columnDefs: systemAdminCols,
+		suppressCsvExport: true,
+		paginationPageSize: this.uiState.filters.pageSize,
+		cacheBlockSize: this.uiState.filters.pageSize,
+		context: { comp: this },
+		defaultColDef: {
+			flex: 1,
+			minWidth: 100,
+			sortable: true,
+			resizable: true,
+		},
+		onGridReady: (e) => this.onGridReady(e),
+		onCellClicked: (e) => this.onCellClicked(e),
+		onSortChanged: (e) => this.onSort(e),
+		onPaginationChanged: (e) => this.onPageChange(e),
+	};
 
+	constructor(
+		private systemAdminService: SystemAdminService,
+		private masterService: MasterMethodsService,
+		private tableRef: ElementRef,
+		private message: MessagesService,
+		private offcanvasService: NgbOffcanvas,
+		private table: MasterTableService,
+		private appUtils: AppUtils,
+		private eventService: EventService,
+		private modalService: NgbModal
+	) {}
 
-  subscribes: Subscription[] = [];
-  gridApi: GridApi = <GridApi> {};
-  gridOpts: GridOptions = {
-    pagination: true,
-    rowModelType: "infinite",
-    editType: "fullRow",
-    animateRows: true,
-    columnDefs: systemAdminCols,
-    suppressCsvExport: true,
-    paginationPageSize: this.uiState.filters.pageSize,
-    cacheBlockSize: this.uiState.filters.pageSize,
-    context: { comp: this },
-    defaultColDef: {
-      flex: 1,
-      minWidth: 100,
-      sortable: true,
-      resizable: true,
-    },
-    onGridReady: (e) => this.onGridReady(e),
-    onCellClicked: (e) => this.onCellClicked(e),
-    onSortChanged: (e) => this.onSort(e),
-    onPaginationChanged: (e) => this.onPageChange(e),
-  };
+	ngOnInit(): void {
+		this.initFilterForm();
+		this.initUserForm();
+		this.getLookupData();
+	}
 
-  constructor (
-    private systemAdminService: SystemAdminService,
-    private masterService: MasterMethodsService,
-    private tableRef: ElementRef,
-    private message: MessagesService,
-    private offcanvasService: NgbOffcanvas,
-    private table: MasterTableService,
-    private appUtils: AppUtils,
-  ) { }
+	dataSource: IDatasource = {
+		getRows: (params: IGetRowsParams) => {
+			this.gridApi.showLoadingOverlay();
+			let sub = this.systemAdminService.getAllAdmins(this.uiState.filters).subscribe(
+				(res: HttpResponse<IBaseResponse<ISystemAdmin[]>>) => {
+					this.uiState.admins.totalPages = JSON.parse(res.headers.get("x-pagination")!).TotalCount;
 
-  ngOnInit (): void
-  {
-    this.initFilterForm();
-    this.getLookupData();
-  }
+					this.uiState.admins.list = res.body?.data!;
+					params.successCallback(this.uiState.admins.list, this.uiState.admins.totalPages);
+					this.uiState.gridReady = true;
+					this.gridApi.hideOverlay();
+				},
+				(err: HttpErrorResponse) => {
+					this.message.popup("Oops!", err.message, "error");
+				}
+			);
+			this.subscribes.push(sub);
+		},
+	};
 
-  dataSource: IDatasource = {
-    getRows: (params: IGetRowsParams) =>
-    {
-      this.gridApi.showLoadingOverlay();
-      let sub = this.systemAdminService.getAllAdmins(this.uiState.filters).subscribe(
-        (res: HttpResponse<IBaseResponse<ISystemAdmin[]>>) =>
-        {
-          this.uiState.admins.totalPages = JSON.parse(res.headers.get("x-pagination")!).TotalCount;
+	onSort(e: GridReadyEvent) {
+		let colState = e.columnApi.getColumnState();
+		colState.forEach((el) => {
+			if (el.sort) {
+				this.uiState.filters.orderBy = el.colId!;
+				this.uiState.filters.orderDir = el.sort!;
+			}
+		});
+	}
 
-          this.uiState.admins.list = res.body?.data!;
-          params.successCallback(this.uiState.admins.list, this.uiState.admins.totalPages);
-          this.uiState.gridReady = true;
-          this.gridApi.hideOverlay();
-        },
-        (err: HttpErrorResponse) =>
-        {
-          this.message.popup("Oops!", err.message, "error");
-        }
-      );
-      this.subscribes.push(sub);
-    },
-  };
+	onCellClicked(params: CellEvent) {
+		if (params.column.getColId() == "action") {
+			params.api.getCellRendererInstances({
+				rowNodes: [params.node],
+				columns: [params.column],
+			});
+		}
+	}
 
-  onSort (e: GridReadyEvent)
-  {
-    let colState = e.columnApi.getColumnState();
-    colState.forEach((el) =>
-    {
-      if (el.sort)
-      {
-        this.uiState.filters.orderBy = el.colId!;
-        this.uiState.filters.orderDir = el.sort!;
-      }
-    });
-  }
+	onPageSizeChange() {
+		this.gridApi.paginationSetPageSize(+this.uiState.filters.pageSize);
+		this.gridOpts.cacheBlockSize = +this.uiState.filters.pageSize;
+		this.gridApi.showLoadingOverlay();
+		this.gridApi.setDatasource(this.dataSource);
+	}
 
-  onCellClicked (params: CellEvent)
-  {
-    if (params.column.getColId() == "action")
-    {
-      params.api.getCellRendererInstances({
-        rowNodes: [ params.node ],
-        columns: [ params.column ],
-      });
-    }
-  }
+	onPageChange(params: GridReadyEvent) {
+		if (this.uiState.gridReady) {
+			this.uiState.filters.pageNumber = this.gridApi.paginationGetCurrentPage() + 1;
+		}
+	}
 
-  onPageSizeChange ()
-  {
-    this.gridApi.paginationSetPageSize(+this.uiState.filters.pageSize);
-    this.gridOpts.cacheBlockSize = +this.uiState.filters.pageSize;
-    this.gridApi.showLoadingOverlay();
-    this.gridApi.setDatasource(this.dataSource);
-  }
+	onGridReady(param: GridReadyEvent) {
+		this.gridApi = param.api;
+		this.gridApi.setDatasource(this.dataSource);
+		// this.gridApi.sizeColumnsToFit();
 
-  onPageChange (params: GridReadyEvent)
-  {
-    if (this.uiState.gridReady)
-    {
-      this.uiState.filters.pageNumber = this.gridApi.paginationGetCurrentPage() + 1;
-    }
-  }
+		const agBodyHorizontalViewport: HTMLElement = this.tableRef.nativeElement.querySelector("#gridScrollbar .ag-body-horizontal-scroll-viewport");
+		const agBodyViewport: HTMLElement = this.tableRef.nativeElement.querySelector("#gridScrollbar .ag-body-viewport");
 
-  onGridReady (param: GridReadyEvent)
-  {
-    this.gridApi = param.api;
-    this.gridApi.setDatasource(this.dataSource);
-    // this.gridApi.sizeColumnsToFit();
+		if (agBodyViewport) {
+			const vertical = new PerfectScrollbar(agBodyViewport);
+			vertical.update();
+		}
+		if (agBodyHorizontalViewport) {
+			const horizontal = new PerfectScrollbar(agBodyHorizontalViewport);
+			horizontal.update();
+		}
+		if ((this, this.uiState.admins.list.length > 0)) this.gridApi.sizeColumnsToFit();
+	}
 
-    const agBodyHorizontalViewport: HTMLElement = this.tableRef.nativeElement.querySelector("#gridScrollbar .ag-body-horizontal-scroll-viewport");
-    const agBodyViewport: HTMLElement = this.tableRef.nativeElement.querySelector("#gridScrollbar .ag-body-viewport");
+	//#region Filter INIT and Functions
+	openSysyemAdminFilter() {
+		this.offcanvasService.open(this.policiesFilter, { position: "end" });
+	}
 
-    if (agBodyViewport)
-    {
-      const vertical = new PerfectScrollbar(agBodyViewport);
-      vertical.update();
-    }
-    if (agBodyHorizontalViewport)
-    {
-      const horizontal = new PerfectScrollbar(agBodyHorizontalViewport);
-      horizontal.update();
-    }
-    if ((this, this.uiState.admins.list.length > 0)) this.gridApi.sizeColumnsToFit();
-  }
+	private initFilterForm(): void {
+		this.filterForm = new FormGroup({
+			fullName: new FormControl(""),
+			branch: new FormControl(""),
+			jobTitle: new FormControl(""),
+			status: new FormControl([]),
+		});
+	}
 
-  //#region Filter INIT and Functions
-  openSysyemAdminFilter ()
-  {
-    this.offcanvasService.open(this.policiesFilter, { position: "end" });
-  }
+	get f() {
+		return this.filterForm.controls;
+	}
 
-  private initFilterForm (): void
-  {
-    this.filterForm = new FormGroup({
-      fullName: new FormControl(""),
-      branch: new FormControl(""),
-      jobTitle: new FormControl(""),
-      status: new FormControl([])
-    });
-  }
+	getLookupData() {
+		this.lookupData = this.table.getBaseData(MODULES.SystemAdmin);
+	}
 
-  get f ()
-  {
-    return this.filterForm.controls;
-  }
+	modifyFilterReq() {
+		this.uiState.filters = {
+			...this.uiState.filters,
+			...this.filterForm.value,
+		};
+	}
 
-  getLookupData ()
-  {
-    this.lookupData = this.table.getBaseData(MODULES.SystemAdmin);
-  }
+	onSystemAdminFilter(): void {
+		this.modifyFilterReq();
+		this.gridApi.setDatasource(this.dataSource);
+	}
 
-  modifyFilterReq ()
-  {
-    this.uiState.filters = {
-      ...this.uiState.filters,
-      ...this.filterForm.value,
-    };
-  }
+	clearFilter() {
+		this.filterForm.reset();
+	}
+	//#endregion
 
-  onSystemAdminFilter (): void
-  {
-    this.modifyFilterReq();
-    this.gridApi.setDatasource(this.dataSource);
-  }
+	//#region Add/Edit User Modal
 
-  clearFilter ()
-  {
-    this.filterForm.reset();
-  }
-  //#endregion
+	userModal!: NgbModalRef;
+	userForm!: FormGroup;
+	userFormSubmitted = false as boolean;
 
-  ngOnDestroy (): void
-  {
-    this.subscribes && this.subscribes.forEach((s) => s.unsubscribe());
-  }
+	initUserForm() {
+		this.userForm = new FormGroup<UserModel>({
+			sno: new FormControl(null),
+			staffId: new FormControl(null),
+			fullName: new FormControl(null),
+			userName: new FormControl(null),
+			jobTitle: new FormControl(null),
+			phoneNo: new FormControl(null),
+			email: new FormControl(null),
+			branch: new FormControl(null),
+			pass: new FormControl(null),
+			savedUser: new FormControl(null),
+			savedDate: new FormControl(null),
+			updateUser: new FormControl(null),
+			updateDate: new FormControl(null),
+			securityRoles: new FormArray([new FormControl()]),
+		});
+	}
+
+	get ff() {
+		return this.userForm.controls;
+	}
+
+	openUsersDialoge(content: TemplateRef<any>) {
+		this.userForm.reset();
+		this.userModal = this.modalService.open(content, {
+			ariaLabelledBy: "modal-basic-title",
+			centered: true,
+			backdrop: "static",
+			size: "xl",
+		});
+
+		this.userModal.hidden.subscribe(() => {
+			this.userForm.reset();
+			this.userFormSubmitted = false;
+		});
+	}
+
+	submitUserData(form: FormGroup) {}
+
+	//#endregion
+
+	ngOnDestroy(): void {
+		this.subscribes && this.subscribes.forEach((s) => s.unsubscribe());
+	}
 }
