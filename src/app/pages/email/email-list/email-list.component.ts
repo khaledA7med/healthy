c79785715f8c2f3245d32a94fb13b8543d375789
Hassan <h.sabry@oasisoft.net>
2023-02-25 22:46:13 +0200

    [Update] Email List

--- src/app/pages/email/email-list/email-list.component.ts
+++ src/app/pages/email/email-list/email-list.component.ts
@@ -1,15 +1,131 @@
-import { Component, OnInit } from '@angular/core';
+import { IEmailData } from "./../../../shared/app/models/Email/email-data";
+import {
+  AfterViewInit,
+  ElementRef,
+  HostListener,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-email-list',
-  templateUrl: './email-list.component.html',
-  styleUrls: ['./email-list.component.scss']
+  selector: "app-email-list",
+  templateUrl: "./email-list.component.html",
+  styleUrls: ["./email-list.component.scss"],
 })
-export class EmailListComponent implements OnInit {
+export class EmailListComponent implements OnInit, AfterViewInit {
+  uiState = {
+    isMenuBar: false,
+    isPrimary: true,
+    isSocial: false,
+    isPromotions: false,
+    allMailList: [] as IEmailData[],
+    socialMailList: [] as IEmailData[],
+    promotionMailList: [] as IEmailData[],
+  };
+  data = {
+    primary: [
+      {
+        id: 1,
+        date: "Mar 7",
+        description: "Trip home from Colombo has been arranged",
+        labeltype: "Support",
+        name: "Peter, me",
+        readed: true,
+        starred: true,
+        tabtype: "Inbox",
+        title: "Hello",
+        userImg: "/assets/images/users/avatar-2.jpg",
+      },
+    ],
+    social: [
+      {
+        id: 2,
+        date: "Mar 8",
+        description: "Trip home from Colombo has been arranged Social",
+        labeltype: "Support",
+        name: "Peter, me",
+        readed: true,
+        starred: false,
+        tabtype: "Inbox",
+        title: "Hello",
+        userImg: "/assets/images/users/avatar-2.jpg",
+      },
+    ],
+    promotion: [
+      {
+        id: 3,
+        date: "Mar 9",
+        description: "Trip home from Colombo has been arranged promotion",
+        labeltype: "Support",
+        name: "Peter, me",
+        readed: true,
+        starred: true,
+        tabtype: "inbox",
+        title: "Hello",
+        userImg: "/assets/images/users/avatar-2.jpg",
+      },
+    ],
+  };
 
-  constructor() { }
+  @ViewChild("sideBar") sideBar!: ElementRef;
+  @ViewChildren("tabList") tabList!: QueryList<HTMLButtonElement>;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.uiState.allMailList = this.data?.primary;
+  }
+  //#region side Bar
+  showSideBar() {
+    let menuBarShow =
+      this.sideBar.nativeElement.classList.contains("menubar-show");
+    if (!menuBarShow) this.sideBar.nativeElement.classList.add("menubar-show");
+    this.uiState.isMenuBar = true;
+  }
+  @HostListener("document:click")
+  Click() {
+    let menuBarShow =
+      this.sideBar.nativeElement.classList.contains("menubar-show");
+    if (menuBarShow) {
+      if (!this.uiState.isMenuBar)
+        this.sideBar.nativeElement.classList.remove("menubar-show");
+      this.uiState.isMenuBar = false;
+    }
+  }
+  //#endregion
+  //#region tab list
+  ngAfterViewInit(): void {
+    console.log(this.tabList);
+  }
+  toggleNavItem(name: string) {
+    switch (name) {
+      case "primary":
+        this.uiState.isPrimary = true;
+        this.uiState.isSocial = false;
+        this.uiState.isPromotions = false;
+        this.uiState.allMailList = this.data.primary;
+
+        break;
+      case "social":
+        this.uiState.isPrimary = false;
+        this.uiState.isSocial = true;
+        this.uiState.isPromotions = false;
+        this.uiState.allMailList = this.data.social;
+
+        break;
+      case "promotions":
+        this.uiState.isPrimary = false;
+        this.uiState.isSocial = false;
+        this.uiState.isPromotions = true;
+        this.uiState.allMailList = this.data.promotion;
+
+        break;
+      default:
+        return;
+    }
   }
 
+  //#endregion
 }
