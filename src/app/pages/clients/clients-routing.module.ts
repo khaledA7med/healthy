54994a2788954f61cc799ac9817198a89aea39d5
Mohamed Farghal <mohamedfarghal62@outlook.com>
2023-03-17 21:28:28 +0200

    [Add] Permissions to Production

--- src/app/pages/clients/clients-routing.module.ts
+++ src/app/pages/clients/clients-routing.module.ts
@@ -1,66 +1,118 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { map } from "rxjs/operators";
-import { ClientFormGuard, ClientGuard } from "src/app/core/guards/clients/client.guard";
+import {
+  ClientFormGuard,
+  ClientGuard,
+} from "src/app/core/guards/clients/client.guard";
 import { ClientsPermissions } from "src/app/core/roles/clients-permissions";
 import { AppRoutes } from "src/app/shared/app/routers/appRouters";
 
 const routes: Routes = [
-	{
-		path: AppRoutes.Client.clientRegistry,
-		data: {
-			title: "Clients Registry",
-		},
-		loadChildren: () => import("./client-registry-list/client-registry-list.module").then((m) => m.ClientRegistryListModule),
-		canActivate: [() => ClientGuard([ClientsPermissions.ChClientsRegistryAdmin, ClientsPermissions.ChClientsRegistry])],
-	},
-	{
-		path: AppRoutes.Client.clientRegistry + "/:id",
-		outlet: "details",
-		loadChildren: () => import("./../../shared/components/client-preview/client-preview.module").then((m) => m.ClientPreviewModule),
-		canActivate: [() => ClientGuard([ClientsPermissions.ChClientsRegistryAdmin, ClientsPermissions.ChClientsRegistry])],
-	},
-	{
-		path: AppRoutes.Client.clientForms,
-		data: {
-			title: "Create Client",
-		},
-		loadChildren: () => import("./client-registry-forms/client-registry-forms.module").then((m) => m.ClientRegistryFormsModule),
-		canActivate: [() => ClientFormGuard([ClientsPermissions.ChClientsRegistryAdmin, ClientsPermissions.ChClientsRegistryAdministratorReadOnly])],
-	},
-	{
-		path: AppRoutes.Client.clientEdit + ":id",
-		data: {
-			title: "Update Client",
-		},
-		loadChildren: () => import("./client-registry-forms/client-registry-forms.module").then((m) => m.ClientRegistryFormsModule),
-		canActivate: [() => ClientFormGuard([ClientsPermissions.ChClientsRegistryAdmin, ClientsPermissions.ChClientsRegistryAdministratorReadOnly])],
-	},
-	{
-		path: AppRoutes.Client.groups,
-		data: {
-			title: "Client Groups",
-		},
-		loadChildren: () => import("./client-group/client-group.module").then((m) => m.ClientGroupModule),
-		canActivate: [() => ClientGuard([ClientsPermissions.ChClientsRegistryAdmin, ClientsPermissions.ChClientsGrouping])],
-	},
-	{
-		path: AppRoutes.Client.reports,
-		data: {
-			title: "Client Reports",
-		},
-		loadChildren: () => import("./client-reports/client-reports.module").then((m) => m.ClientReportsModule),
-		canActivate: [() => ClientGuard([ClientsPermissions.ChClientsRegistryAdmin])],
-	},
-	{
-		path: "",
-		redirectTo: "/",
-		pathMatch: "full",
-	},
+  {
+    path: AppRoutes.Client.clientRegistry,
+    data: {
+      title: "Clients Registry",
+    },
+    loadChildren: () =>
+      import("./client-registry-list/client-registry-list.module").then(
+        (m) => m.ClientRegistryListModule
+      ),
+    canActivate: [
+      () =>
+        ClientGuard([
+          ClientsPermissions.ChClientsRegistryAdmin,
+          ClientsPermissions.ChClientsRegistry,
+        ]),
+    ],
+  },
+  {
+    path: AppRoutes.Client.clientRegistry + "/:id",
+    outlet: "details",
+    loadChildren: () =>
+      import(
+        "./../../shared/components/client-preview/client-preview.module"
+      ).then((m) => m.ClientPreviewModule),
+    canActivate: [
+      () =>
+        ClientGuard([
+          ClientsPermissions.ChClientsRegistryAdmin,
+          ClientsPermissions.ChClientsRegistry,
+        ]),
+    ],
+  },
+  {
+    path: AppRoutes.Client.clientForms,
+    data: {
+      title: "Create Client",
+    },
+    loadChildren: () =>
+      import("./client-registry-forms/client-registry-forms.module").then(
+        (m) => m.ClientRegistryFormsModule
+      ),
+    canActivate: [
+      () =>
+        ClientFormGuard([
+          ClientsPermissions.ChClientsRegistryAdmin,
+          ClientsPermissions.ChClientsRegistryAdministratorReadOnly,
+        ]),
+    ],
+  },
+  {
+    path: AppRoutes.Client.clientEdit + ":id",
+    data: {
+      title: "Update Client",
+    },
+    loadChildren: () =>
+      import("./client-registry-forms/client-registry-forms.module").then(
+        (m) => m.ClientRegistryFormsModule
+      ),
+    canActivate: [
+      () =>
+        ClientFormGuard([
+          ClientsPermissions.ChClientsRegistryAdmin,
+          ClientsPermissions.ChClientsRegistryAdministratorReadOnly,
+        ]),
+    ],
+  },
+  {
+    path: AppRoutes.Client.groups,
+    data: {
+      title: "Client Groups",
+    },
+    loadChildren: () =>
+      import("./client-group/client-group.module").then(
+        (m) => m.ClientGroupModule
+      ),
+    canActivate: [
+      () =>
+        ClientGuard([
+          ClientsPermissions.ChClientsRegistryAdmin,
+          ClientsPermissions.ChClientsGrouping,
+        ]),
+    ],
+  },
+  {
+    path: AppRoutes.Client.reports,
+    data: {
+      title: "Client Reports",
+    },
+    loadChildren: () =>
+      import("./client-reports/client-reports.module").then(
+        (m) => m.ClientReportsModule
+      ),
+    canActivate: [
+      () => ClientGuard([ClientsPermissions.ChClientsRegistryAdmin]),
+    ],
+  },
+  {
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forChild(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class ClientsRoutingModule {}
