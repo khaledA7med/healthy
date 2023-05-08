0fa36ed544800c5fdfeb4f345a14c22195ae2eb8
Khalid Ahmed Ahmed <khalid.ahmed@oasisoft.net>
2023-05-08 14:13:49 +0300

    [add] fix bug in master table

--- src/app/shared/services/master-tables/customer-service/insurance-companies-documents.service.ts
+++ src/app/shared/services/master-tables/customer-service/insurance-companies-documents.service.ts
@@ -1,36 +1,49 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IBaseResponse } from 'src/app/shared/app/models/App/IBaseResponse';
-import { IDocumentReq } from 'src/app/shared/app/models/App/IDocumentReq';
-import { IInsuranceCompaniesDocuments, IInsuranceCompaniesDocumentsData } from 'src/app/shared/app/models/MasterTables/customer-service/i-insurance-companies-documents';
-import { ApiRoutes } from 'src/app/shared/app/routers/ApiRoutes';
-import { environment } from 'src/environments/environment';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { IBaseResponse } from "src/app/shared/app/models/App/IBaseResponse";
+import { IDocumentReq } from "src/app/shared/app/models/App/IDocumentReq";
+import {
+  IInsuranceCompaniesDocuments,
+  IInsuranceCompaniesDocumentsData,
+} from "src/app/shared/app/models/MasterTables/customer-service/i-insurance-companies-documents";
+import { ApiRoutes } from "src/app/shared/app/routers/ApiRoutes";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class InsuranceCompaniesDocumentsService
-{
-
+export class InsuranceCompaniesDocumentsService {
   private readonly env = environment.baseURL;
-  constructor (private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-  getInsuranceCompaniesDocuments (data: IInsuranceCompaniesDocumentsData): Observable<HttpResponse<IBaseResponse<IInsuranceCompaniesDocuments[]>>>
-  {
-    return this.http.post<IBaseResponse<IInsuranceCompaniesDocuments[]>>(this.env + ApiRoutes.masterTables.customerService.InsuranceCompaniesDocuments.search, data, { observe: "response" });
+  getInsuranceCompaniesDocuments(
+    data: IInsuranceCompaniesDocumentsData
+  ): Observable<HttpResponse<IBaseResponse<IInsuranceCompaniesDocuments[]>>> {
+    return this.http.post<IBaseResponse<IInsuranceCompaniesDocuments[]>>(
+      this.env +
+        ApiRoutes.masterTables.customerService.InsuranceCompaniesDocuments
+          .search,
+      data,
+      { observe: "response" }
+    );
   }
 
-  uploadInsuranceCompaniesDocuments (data: FormData): Observable<HttpResponse<IBaseResponse<number>>>
-  {
-    return this.http.post<IBaseResponse<number>>(this.env + ApiRoutes.masterTables.customerService.InsuranceCompaniesDocuments.upload, data, {
-      observe: "response",
-    });
+  uploadInsuranceCompaniesDocuments(
+    data: FormData
+  ): Observable<HttpResponse<IBaseResponse<number>>> {
+    return this.http.post<IBaseResponse<number>>(
+      this.env +
+        ApiRoutes.masterTables.customerService.InsuranceCompaniesDocuments
+          .upload,
+      data,
+      {
+        observe: "response",
+      }
+    );
   }
 
-  downloadDocument (data: IDocumentReq): Observable<HttpResponse<any>>
-  {
+  downloadDocument(data: IDocumentReq): Observable<HttpResponse<any>> {
     return this.http.post(
       this.env + ApiRoutes.MasterMethods.downloadDocument,
       { path: data },
@@ -38,13 +51,12 @@ export class InsuranceCompaniesDocumentsService
     );
   }
 
-  deleteDocument (
+  deleteDocument(
     data: IDocumentReq
-  ): Observable<HttpResponse<IBaseResponse<null>>>
-  {
+  ): Observable<HttpResponse<IBaseResponse<null>>> {
     return this.http.post<IBaseResponse<null>>(
       this.env + ApiRoutes.MasterMethods.deleteDocument,
-      { path: data },
+      data,
       { observe: "response" }
     );
   }
